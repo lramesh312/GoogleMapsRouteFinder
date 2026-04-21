06e1fb618518e13493d30e182dbc510d1892692d
lramesh312 <115879522+lramesh312@users.noreply.github.com>
2026-04-21 09:58:35 -0400

    Initialize SubsetBST in HashArray constructor

--- src/com/lalithsoftware/finally/HashArray.java
+++ src/com/lalithsoftware/finally/HashArray.java
@@ -1,8 +1,12 @@
+package com.lalithsoftware.finally;
 
 public class HashArray
 {
+    private SubsetBST subsetTree = null;
+    
     public HashArray()
     {
+        subsetTree = new SubsetBST();
     }
   
 }
