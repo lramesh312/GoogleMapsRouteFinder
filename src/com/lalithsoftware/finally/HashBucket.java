a303925505fe6e2521a4efe9519b23211a7369eb
lramesh312 <115879522+lramesh312@users.noreply.github.com>
2026-04-25 14:49:16 -0400

    Create HashBucket.java

---
+++ src/com/lalithsoftware/finally/HashBucket.java
@@ -0,0 +1,13 @@
+package com.lalithsoftware.finally;
+
+public class BucketList
+  {
+      private BucketList next = null;
+      private HashItem data = null;
+    
+      public BucketList()
+      {
+
+      }
+      
+  }
