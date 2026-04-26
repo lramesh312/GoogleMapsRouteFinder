36997139e8e7b479e02756987224b75183b49736
lramesh312 <115879522+lramesh312@users.noreply.github.com>
2026-04-26 08:45:59 -0400

    Update BucketList.java

--- src/com/lalithsoftware/finally/BucketList.java
+++ src/com/lalithsoftware/finally/BucketList.java
@@ -3,7 +3,6 @@ package com.lalithsoftware.finally;
 public class BucketList
   {
       private BucketList next = null;
-      private HashItem data = null;
     
       public BucketList()
       {
