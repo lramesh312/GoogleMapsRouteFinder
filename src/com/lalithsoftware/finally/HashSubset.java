905ee287c55dcef19f32d0c1b5fb95f87080d375
lramesh312 <115879522+lramesh312@users.noreply.github.com>
2026-05-13 18:36:26 -0400

    Adding size estimate for solution objects.

--- src/com/lalithsoftware/finally/HashSubset.java
+++ src/com/lalithsoftware/finally/HashSubset.java
@@ -4,11 +4,13 @@ public class HashSubset
 {
     private ArrayList<SubsetItem> itemList;
     private Integer size;
+    private Integer sizeEstimate;
 
     public HashSubset()
     {
         itemList = new ArrayList<SubsetItem>();
         size = new Integer(0);
+        sizeEstimate = new Integer(0);
     }
 
     public ArrayList<SubsetItem> getItemList()
@@ -21,6 +23,7 @@ public class HashSubset
         for(int k = 0; k < itemList.size(); k++)
         {
             SubsetItem listItem = itemList.get(k);
+            
         }
         int toReturn = 0;
     }
